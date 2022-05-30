597aada71f5f6ab3ea2b8cf0eb275517985a0bcc
prakash760 <64522716+prakash760@users.noreply.github.com>
2022-05-30 22:57:05 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,9 @@
+import Image from 'next/image';
 import { useRouter } from 'next/router';
-import Image from 'next/image'
-
 import { SearchIcon } from '@heroicons/react/outline';
 
 const Header: React.FC<{}> = () => {
   const router = useRouter();
-
   const handleKeyPress: React.KeyboardEventHandler<HTMLInputElement> = (
     event
   ) => {
@@ -15,7 +13,6 @@ const Header: React.FC<{}> = () => {
 
   return (
     <header className="sticky top-0 z-[51] flex h-12 w-full items-center bg-gray-900 shadow-md">
-
       <div className="ml-4 flex items-center rounded bg-gray-50 py-[1px] px-2 sm:ml-6">
          <Image src="/photo_2022-05-30_22-48-44.jpg" />
         <SearchIcon className="h-4 w-4" />
